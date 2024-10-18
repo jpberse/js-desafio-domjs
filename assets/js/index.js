f280e63deb1a588f9a549afec5d49bb9a28237fb
jpberse <jp.bersezio@gmail.com>
2024-10-18 19:39:43 -0300

    Se mueven variables a archivo js y se agrega condicion para que el boton - no de valores negativos

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,7 @@
-precio = 400000
+const span = document.querySelector('.cantidad');
+const valor = document.querySelector('.valor-total');
+let precio = 400000
+let cantidad = Number(span.innerHTML);
 
 precioSpan = document.querySelector(".precio-inicial");
 precioSpan.innerHTML = precio
